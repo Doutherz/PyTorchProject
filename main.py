13edda1c745ec70b416a2e860df3294fce2d60dd
Matt <douther0z@gmail.com>
2024-02-03 21:28:37 +0000

    Defined neural network

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ testData = pd.read_csv("sales_data_test.csv")
 # split data into x and y for the nn
 xTrainData = trainData.drop("total_earnings", axis=1)
 yTrainData = trainData["total_earnings"]
-
+print(xTrainData.to_string())
 xTestData = testData.drop("total_earnings", axis=1)
 yTestData = testData["total_earnings"]
 
@@ -19,3 +19,22 @@ yTestData = testData["total_earnings"]
 scaler = MinMaxScaler()
 xTrainData = scaler.fit_transform(xTrainData)
 xTestData = scaler.fit_transform(xTestData)
+
+# neural network model created
+class Net(nn.Module):
+    def __int__(self, inSize, hiddenSize, outSize):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(inSize, hiddenSize)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hiddenSize, outSize)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
+
+inSize = 9
+hiddenSize = 50
+outSize = 1
+model = Net(inSize, hiddenSize, outSize)
