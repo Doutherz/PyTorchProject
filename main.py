e36b1cd13e0b08ba7a6a71660fd07138f48c9ec7
Matt <douther0z@gmail.com>
2024-02-03 21:54:00 +0000

    added model training and testing

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
+import torchmetrics
 from torch.utils.data import DataLoader, TensorDataset
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 
 # load test data
@@ -11,7 +12,7 @@ testData = pd.read_csv("sales_data_test.csv")
 # split data into x and y for the nn
 xTrainData = trainData.drop("total_earnings", axis=1)
 yTrainData = trainData["total_earnings"]
-print(xTrainData.to_string())
+
 xTestData = testData.drop("total_earnings", axis=1)
 yTestData = testData["total_earnings"]
 
@@ -19,6 +20,19 @@ yTestData = testData["total_earnings"]
 scaler = MinMaxScaler()
 xTrainData = scaler.fit_transform(xTrainData)
 xTestData = scaler.fit_transform(xTestData)
+
+xTrainTensor = torch.Tensor(xTrainData)
+yTrainTensor = torch.Tensor(yTrainData)
+trainDataSet = TensorDataset(xTrainTensor, yTrainTensor)
+
+xTestTensor = torch.Tensor(xTestData)
+yTestTensor = torch.Tensor(yTestData)
+testDataSet = TensorDataset(xTestTensor, yTestTensor)
+
+batchSize = 64
+trainLoader = DataLoader(dataset=trainDataSet, batch_size=batchSize, shuffle=True)
+testLoader = DataLoader(dataset=testDataSet, batch_size=batchSize, shuffle=True)
+
 
 # neural network model created
 class Net(nn.Module):
@@ -34,7 +48,36 @@ class Net(nn.Module):
         x = self.fc2(x)
         return x
 
+
 inSize = 9
 hiddenSize = 50
 outSize = 1
 model = Net(inSize, hiddenSize, outSize)
+
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# training loop
+numEpochs = 100
+for epoch in range(numEpochs):
+    for inputs, labels in trainLoader:
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+model.eval()
+with torch.no_grad():
+    allPredictions = []
+    allLabels = []
+    for inputs, labels in testLoader:
+        predictions = model(inputs)
+        allPredictions.append(predictions)
+        allLabels.append(labels)
+
+allPredictions = torch.cat(allPredictions).numpy()
+allLabels = torch.cat(allLabels).numpy()
+
+RSquared = torchmetrics.functional.r2_score(torch.Tensor(allPredictions), torch.Tensor(allLabels))
+print("R-squared: " + RSquared)
